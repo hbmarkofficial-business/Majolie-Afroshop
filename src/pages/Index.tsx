1036a583c98be2901bc92a5c8602269b7cde6214
hbmarkofficial-business <hbmark.official@gmail.com>
2026-02-13 15:16:54 +0100

    Update Index.tsx

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -12,63 +12,63 @@ const Index = () => {
     <>
       <Helmet>
         {/* Titel & Meta Description */}
-        <title>Barbershop Prenzlauer Berg | The Gentleman's Cut</title>
+        <title>Friseursalon Mönchengladbach | Majolie Afroshop</title>
         <meta
           name="description"
-          content="Besuche unseren Premium Barbershop The Gentleman's Cut in Berlin Prenzlauer Berg für erstklassige Haarschnitte, Bartpflege und individuelle Styles. Jetzt Termin buchen!"
+          content="Besuche Majolie Afroshop in Mönchengladbach für erstklassige Haarschnitte, professionelles Braiding und individuelle Styles. Dein Spezialist für Afro-Haarpflege. Jetzt Termin buchen!"
         />
         <meta
           name="keywords"
-          content="Barbershop, Herrenfriseur, Haarschnitt, Bartpflege, Rasur, Berlin, Prenzlauer Berg"
+          content="Friseursalon, Afroshop, Mönchengladbach, Braiding, Haarschnitt, Haarpflege, Herrenfriseur"
         />
-        <link rel="canonical" href="https://gentlemanscut.de" />
+        <link rel="canonical" href="https://majolie-afroshop.de" />
 
         {/* Open Graph */}
-        <meta property="og:title" content="The Gentleman's Cut | Premium Barbershop Berlin" />
+        <meta property="og:title" content="Majolie Afroshop | Premium Friseursalon Mönchengladbach" />
         <meta
           property="og:description"
-          content="Erstklassige Haarschnitte und Bartpflege für den modernen Gentleman in Berlin Prenzlauer Berg."
+          content="Erstklassige Haarschnitte und spezialisierte Haarpflege im modernen Majolie Afroshop in Mönchengladbach."
         />
         <meta property="og:type" content="website" />
-        <meta property="og:url" content="https://gentlemanscut.de" />
-        <meta property="og:image" content="https://gentlemanscut.de/images/og-image.png" />
+        <meta property="og:url" content="https://majolie-afroshop.de" />
+        <meta property="og:image" content="https://majolie-afroshop.de/images/og-image.png" />
 
         {/* Twitter Card */}
         <meta name="twitter:card" content="summary_large_image" />
-        <meta name="twitter:title" content="The Gentleman's Cut | Premium Barbershop Berlin" />
+        <meta name="twitter:title" content="Majolie Afroshop | Premium Friseursalon Mönchengladbach" />
         <meta
           name="twitter:description"
-          content="Erleben Sie erstklassige Haarschnitte und Bartpflege für den modernen Gentleman."
+          content="Erlebe erstklassige Haarschnitte und professionelle Styles bei Majolie Afroshop."
         />
-        <meta name="twitter:image" content="https://gentlemanscut.de/images/og-image.png" />
+        <meta name="twitter:image" content="https://majolie-afroshop.de/images/og-image.png" />
 
         {/* Schema.org LocalBusiness */}
         <script type="application/ld+json">
           {JSON.stringify({
             "@context": "https://schema.org",
-            "@type": "Barbershop",
-            "name": "The Gentleman's Cut",
-            "image": "https://gentlemanscut.de/images/logo.png",
+            "@type": "HairSalon",
+            "name": "Majolie Afroshop",
+            "image": "https://majolie-afroshop.de/images/logo.png",
             "address": {
               "@type": "PostalAddress",
-              "streetAddress": "Straße 12",
-              "addressLocality": "Berlin",
-              "postalCode": "10115",
+              "streetAddress": "Waldhausener Str. 86",
+              "addressLocality": "Mönchengladbach",
+              "postalCode": "41061",
               "addressCountry": "DE"
             },
             "geo": {
               "@type": "GeoCoordinates",
-              "latitude": "52.5200",
-              "longitude": "13.4050"
+              "latitude": "51.1947",
+              "longitude": "6.4297"
             },
-            "url": "https://gentlemanscut.de",
-            "telephone": "+49 30 1234567",
-            "openingHours": "Mo-Fr 09:00-19:00, Sa 09:00-17:00",
+            "url": "https://majolie-afroshop.de",
+            "telephone": "+49 176 24316809",
+            "openingHours": "Di-Fr 10:00-18:00, Sa 09:00-17:00",
             "priceRange": "€€",
-            "servesCuisine": "Barbershop Services",
+            "servesCuisine": "Friseursalon Services",
             "sameAs": [
-              "https://www.facebook.com/gentlemanscut",
-              "https://www.instagram.com/gentlemanscut"
+              "https://www.facebook.com/majolieafroshop",
+              "https://www.instagram.com/majolieafroshop"
             ]
           })}
         </script>
